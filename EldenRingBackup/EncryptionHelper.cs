e46fa85452765fa1decd89a6d62dbc30d68b7e1b
aiqinxuancai <aiqinxuancai@163.com>
2022-03-30 22:42:04 +0800

    添加项目文件。

---
+++ EldenRingBackup/EncryptionHelper.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EldenRingBackup
+{
+    public class EncryptionHelper
+    {
+        public static string GetMD5(string sDataIn)
+        {
+            System.Security.Cryptography.MD5CryptoServiceProvider md5 = new System.Security.Cryptography.MD5CryptoServiceProvider();
+            byte[] bytValue, bytHash;
+            bytValue = System.Text.Encoding.UTF8.GetBytes(sDataIn);
+            bytHash = md5.ComputeHash(bytValue);
+            md5.Clear();
+            string sTemp = "";
+            for (int i = 0; i < bytHash.Length; i++)
+            {
+                sTemp += bytHash[i].ToString("X").PadLeft(2, '0');
+            }
+            return sTemp.ToLower();
+        }
+
+        public static string GetMD5(byte[] sDataIn)
+        {
+            System.Security.Cryptography.MD5CryptoServiceProvider md5 = new System.Security.Cryptography.MD5CryptoServiceProvider();
+            byte[] bytValue, bytHash;
+            bytValue = sDataIn;
+            bytHash = md5.ComputeHash(bytValue);
+            md5.Clear();
+            string sTemp = "";
+            for (int i = 0; i < bytHash.Length; i++)
+            {
+                sTemp += bytHash[i].ToString("X").PadLeft(2, '0');
+            }
+            return sTemp.ToLower();
+        }
+
+        /// <summary>
+        /// AES 算法加密(ECB模式) 将明文加密，加密后进行base64编码，返回密文
+        /// </summary>
+        /// <param name="EncryptStr">明文</param>
+        /// <param name="Key">密钥</param>
+        /// <returns>加密后base64编码的密文</returns>
+        public static string AesEncryptionBase64(string EncryptStr, string Key)
+        {
+            try
+            {
+                //byte[] keyArray = Encoding.UTF8.GetBytes(Key);
+                byte[] keyArray = Convert.FromBase64String(Key);
+                byte[] toEncryptArray = Encoding.UTF8.GetBytes(EncryptStr);
+
+                RijndaelManaged rDel = new RijndaelManaged();
+                rDel.Key = keyArray;
+                rDel.Mode = CipherMode.ECB;
+                rDel.Padding = PaddingMode.PKCS7;
+
+                ICryptoTransform cTransform = rDel.CreateEncryptor();
+                byte[] resultArray = cTransform.TransformFinalBlock(toEncryptArray, 0, toEncryptArray.Length);
+
+                return Convert.ToBase64String(resultArray, 0, resultArray.Length);
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+
+
+        /// <summary>
+        /// AES 算法解密(ECB模式) 将密文base64解码进行解密，返回明文
+        /// </summary>
+        /// <param name="DecryptStr">密文</param>
+        /// <param name="Key">密钥</param>
+        /// <returns>明文</returns>
+        public static string AesDecryptBase64(string DecryptStr, string Key)
+        {
+            try
+            {
+                //byte[] keyArray = Encoding.UTF8.GetBytes(Key);
+                byte[] keyArray = Convert.FromBase64String(Key);
+                byte[] toEncryptArray = Convert.FromBase64String(DecryptStr);
+
+                RijndaelManaged rDel = new RijndaelManaged();
+                rDel.Key = keyArray;
+                rDel.Mode = CipherMode.ECB;
+                rDel.Padding = PaddingMode.PKCS7;
+
+                ICryptoTransform cTransform = rDel.CreateDecryptor();
+                byte[] resultArray = cTransform.TransformFinalBlock(toEncryptArray, 0, toEncryptArray.Length);
+
+                return Encoding.UTF8.GetString(resultArray);//  UTF8Encoding.UTF8.GetString(resultArray);
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+    }
+}
---
+++ EldenRingBackup/Program.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace EldenRingBackup // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+
+            string applicationData = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+            string eldenRingPath = Path.Combine(applicationData, "EldenRing");
+
+            if (Directory.Exists(eldenRingPath))
+            {
+                var result = StartBackup(eldenRingPath);
+                result.Start();
+                
+            }
+            else
+            {
+                Console.WriteLine("未找到EldenRing备份目录");
+            }
+
+            Console.ReadKey();
+
+
+        }
+
+
+        private static async Task<bool> StartBackup(string path)
+        {
+            Console.WriteLine("开始检测文件夹更改");
+            while (true)
+            {
+                if (CheckWrite(path))
+                {
+                    Console.WriteLine("文件夹已更改，开始备份");
+                    BackupDir(path);
+                    UpdateWrite(path);
+
+                }
+                Thread.Sleep(60 * 1000);
+            }
+            return true;
+        }
+
+        /// <summary>
+        /// 检查MD5是否需要备份
+        /// </summary>
+        /// <param name="path"></param>
+        private static bool CheckWrite(string path)
+        {
+            var hashName = EncryptionHelper.GetMD5(path) + "-" + Path.GetFileName(path);
+            if (File.Exists(hashName))
+            {
+                //读入
+                var fileInfo = new DirectoryInfo(path);
+                var time = fileInfo.LastWriteTime.ToString("yyyy-MM-dd-HH-mm-ss");
+                //Console.WriteLine(time);
+
+                if (File.ReadAllText(hashName) != time)
+                {
+                    return true;
+                }
+
+            }
+            else
+            {
+                //生成
+                return true;
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// 更新备份时间
+        /// </summary>
+        /// <param name="path"></param>
+        private static void UpdateWrite(string path)
+        {
+            var hashName = EncryptionHelper.GetMD5(path) + "-" + Path.GetFileName(path);
+            var fileInfo = new DirectoryInfo(path);
+            var time = fileInfo.LastWriteTime.ToString("yyyy-MM-dd-HH-mm-ss");
+            Console.WriteLine("最后备份时间" + time);
+            File.WriteAllText(hashName, time);
+        }
+
+        private static void BackupDir(string path)
+        {
+            string savePath = Path.Combine(Directory.GetCurrentDirectory(), "save") ;
+
+            string timeStr = DateTime.Now.ToString("yyyy-MM-dd-HH-mm-ss");
+
+            string newPath = Path.Combine(savePath, Path.GetFileName(path) + "-" + timeStr);
+
+
+            Console.WriteLine("备份到:" + newPath);
+
+
+            DirectoryCopy(path, newPath, true);
+        }
+
+        private static void DirectoryCopy(string sourceDirName, string destDirName, bool copySubDirs)
+        {
+            // Get the subdirectories for the specified directory.
+            DirectoryInfo dir = new DirectoryInfo(sourceDirName);
+
+            if (!dir.Exists)
+            {
+                throw new DirectoryNotFoundException(
+                    "Source directory does not exist or could not be found: "
+                    + sourceDirName);
+            }
+
+            DirectoryInfo[] dirs = dir.GetDirectories();
+
+            // If the destination directory doesn't exist, create it.       
+            Directory.CreateDirectory(destDirName);
+
+            // Get the files in the directory and copy them to the new location.
+            FileInfo[] files = dir.GetFiles();
+            foreach (FileInfo file in files)
+            {
+                string tempPath = Path.Combine(destDirName, file.Name);
+                file.CopyTo(tempPath, false);
+            }
+
+            // If copying subdirectories, copy them and their contents to new location.
+            if (copySubDirs)
+            {
+                foreach (DirectoryInfo subdir in dirs)
+                {
+                    string tempPath = Path.Combine(destDirName, subdir.Name);
+                    DirectoryCopy(subdir.FullName, tempPath, copySubDirs);
+                }
+            }
+        }
+
+    }
+
+}
+
+
